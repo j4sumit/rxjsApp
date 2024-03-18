ed5c21e01f66f7180401c3856902a5043beff920
Sumit Kumar <j4sumit@gmail.com>
2024-03-18 18:27:49 +0530

    Converted Input Element in Observable|| normal observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,ElementRef } from '@angular/core';
+import {Observable, fromEvent} from "rxjs"
+import {filter, map, tap} from "rxjs/operators"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("search",{static:true})  //created observable for input Element1
+  search?: ElementRef<HTMLInputElement>//created observable for input Element2
+
   title = 'rxjsApp';
+
+
+  ngOnInit(){
+
+const searchObs = fromEvent(this.search!.nativeElement,"input")//created observable for input Element3
+searchObs.subscribe((value:any)=>{//created observable for input Element4
+  console.log(value)//created observable for input Element5
+})
+
+    // Observable-> who emits data
+const pizzaObservable = new Observable((subscriber)=>{
+subscriber.next({name:"Farm Houser", veg:true, size:"small"})
+subscriber.next({name:"Margherita", veg:true, size:"large"})
+subscriber.next({name:"barberqueChicket", veg:false,size:"medium"})
+subscriber.complete()
+subscriber.next({name:"Margherita", veg:true})
+}).pipe(
+  tap((pizza:any)=>
+  {//Side Effects
+    if(pizza.size=="large"){
+    throw new Error("large Size pizza not allowed")
+  }
+}
+  ),
+  filter((pizza:any)=>{return pizza.veg==true}),
+  map((pizza:any)=> pizza.name)
+)
+//Subscriber/observer=> who consumes emited data
+// pizzaObservable.subscribe({
+//   next:(value)=>console.log(value),
+//   error:(err)=>console.log(err.message),
+//   complete:()=>console.log("i am done eating pizza")
+// })
+
+  }
 }
